67e31449a461398565ef4f6d9969ac5ee611ef0e
Abhishek Gupta <86161191+Im-Abhi@users.noreply.github.com>
2025-04-22 16:57:21 +0530

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+myname="test"
+myage="20"
+
+echo "Hello, my name is $myname."
+echo "I'm $myage years old."
+
+now=$(date)
+echo "System date, time is: "
+echo $now
+
+echo "Your username is $USER"

e883a36969791b257411c9118e2f07d84c309061
Abhishek Gupta <86161191+Im-Abhi@users.noreply.github.com>
2025-04-24 13:23:04 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -6,6 +6,7 @@ myage="20"
 echo "Hello, my name is $myname."
 echo "I'm $myage years old."
 
+# $(...)   this creates a subshell using which we can get the result of a command
 now=$(date)
 echo "System date, time is: "
 echo $now

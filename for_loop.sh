b91452beca82bd0d9cdd44eacc3786ae9e9d563b
Abhishek Gupta <86161191+Im-Abhi@users.noreply.github.com>
2025-04-25 12:12:50 +0530

    Create for_loop.sh

---
+++ for_loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# for current_number in {1..10}
+for current_number in 1 2 3 4 5
+do
+        echo $current_number
+        sleep 1
+done
+
+for file in logfiles/*.log
+do
+        # do something
+done
